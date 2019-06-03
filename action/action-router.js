58e72f76e33ddfd42c8081fc609cb78bb02ffc52
Antony Nyagah <anttonynyagah@Antonys-MBP-2.attlocal.net>
2019-06-02 23:39:22 -0400

    action router updated

--- action/action-router.js
+++ action/action-router.js
@@ -14,9 +14,28 @@ const db = knex(knexConfig);
 
 //check
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+  db('actions')
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
-router.post
+router.post('/', async (req, res) => {
+  try {
+    const action = await db('actions').insert(req.body)
+    res.status(201).json(action)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'There was an error posting that!' })
+    console.log(error)
 
-module.exports = router; 
+  }
+});
+
+
+
+
+module.exports = router;
