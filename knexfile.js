2211ad341ad204ac0a2060dcab3277c46ae08e8b
Antony Nyagah <anttonynyagah@Antonys-MBP-2.attlocal.net>
2019-06-02 23:42:01 -0400

    knex update

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,11 @@
+// Update with your config settings.
 module.exports = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/lambda.db3', // the folder will be created when we run the migrations
+      filename: './data/dbproject.db3'
     },
-    pool: {
-      // runs after a connection is made to the sqlite engine
-      afterCreate: (conn, done) => {
-        // by default SQLite will not enforce Foreign Keys
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-      },
-    },
-    migrations: {
-      // we can change the location of the migrations
-      directory: './data/migrations',
-    },
-    seeds: {
-      // we can change the location of the seeds
-      directory: './data/seeds',
-    },
+    useNullAsDefault: true,
   },
 };
