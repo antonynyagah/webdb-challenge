c22cd1e7b5758c7c1cb93813932e9901e9c851a1
Antony Nyagah <anttonynyagah@Antonys-MBP-2.attlocal.net>
2019-06-02 23:41:28 -0400

    project router update

--- project/project-router.js
+++ project/project-router.js
@@ -13,13 +13,42 @@ const knexConfig = {
 const db = knex(knexConfig);
 
 //check
-router.get('/', (req, res) => {
-  res.send('Hello World!')
+router.get('/', async (req, res) => {
+  try {
+    const projects = await db('projects');
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post
+router.post('/', async (req, res) => {
+  try {
+    const [id] = await db('projects').insert(req.body);
 
+    const project = await db('projects')
+      .where({ id })
+      .first();
 
+    res.status(201).json(project);
+  } catch (error) {
+    const message = errors[error.errno] || 'We ran into an error';
+    res.status(500).json({ message, error });
+  }
+});
 
+router.get('/project/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id: id })
+    .first()
+    .then(projects => {
+      db('actions')
+        .where({ project_id: id }).then(actions => {
+        (projects.actions = actions);
+        return res.status(200).json(projects);
+      });
+    })
+});
 
 module.exports = router;
